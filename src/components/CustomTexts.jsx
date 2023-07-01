aea8810226b490bb28cc57081de80b54ac6aa0de
Salman Hamza <90480347+salmanhamza@users.noreply.github.com>
2023-07-02 00:26:05 +0500

    updating about section

--- src/components/CustomTexts.jsx
+++ src/components/CustomTexts.jsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+import { textContainer } from "../utils/motion";
+
+export const TypingText = ({ title, textStyle }) => (
+  <motion.p variants={textContainer} initial="hidden"></motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyle }) => <h2>Title Text</h2>;
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,25 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+
+import { TypingText } from "../components/CustomTexts";
+
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Metaversus" textStyle="text-center" />
+    </motion.div>
     About section
   </section>
 );
